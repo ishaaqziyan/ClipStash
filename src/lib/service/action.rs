50e7b3c2da378b74935c4134d236425fcbb1fe07
Ishaaq Ziyan <ishaaqziyan@gmail.com>
2023-08-12 22:02:50 +0530

    Deployed functionality for the Service layer and Update clip

--- src/lib/service/action.rs
+++ src/lib/service/action.rs
@@ -1,7 +1,19 @@
+use crate::data::model::NewClip;
 use crate::data::{query,DatabasePool,Transaction};
 use crate::service::ask;
 use crate::{Clip,ServiceError,ShortCode};
 use std::convert::TryInto;
+
+
+pub async fn new_clip(req: ask::NewClip, pool: &DatabasePool)
+-> Result<Clip,ServiceError> {
+       Ok(query::new_clip(req, pool).await?.try_into()?)
+}
+
+pub async fn update_clip(req: ask::UpdateClip, pool: &DatabasePool)
+-> Result<Clip,ServiceError> {
+       Ok(query::update_clip(req, pool).await?.try_into()?)
+}
 
 pub async fn get_clip (req: ask::GetClip, pool: &DatabasePool) 
 -> Result<Clip, ServiceError> {
