a4d9c967339de20101fdfa764868503c0d2fe350
Ishaaq Ziyan <ishaaqziyan@gmail.com>
2023-08-18 14:15:47 +0530

    Deployed Request Guard for API

--- src/lib/web/api.rs
+++ src/lib/web/api.rs
@@ -81,3 +81,45 @@ impl From<ServiceError>  for ApiError {
         }
     }
 }
+
+#[rocket::async_trait]
+impl <'r> FromRequest<'r> for ApiKey{
+   type Error = ApiError;
+
+   async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+
+    fn server_error() -> Outcome<ApiKey,ApiError>{
+        Outcome::Failure((
+            Status::InternalServerError,
+            ApiError::Server(Json("Server Error".to_string())),
+        ))
+    }
+
+    fn key_error(e: ApiKeyError) -> Outcome<ApiKey,ApiError>{
+        Outcome::Failure((
+            Status::BadRequest,
+            ApiError::KeyError(Json(e)),
+        ))
+    }
+    match req.headers().get_one(API_KEY_HEADER) {
+        None => key_error( ApiKeyError::NotFound("Api Key not found".to_string())),
+        Some(key) => {
+            let db = match req.guard::<&State<AppDatabase>>().await{
+                Outcome::Success(db) => db,
+                _=> return server_error(),
+            };
+            let api_key =match ApiKey::from_str(key){
+                Ok(key) => key,
+                Err(e) => return key_error(e),
+            };
+            match action::api_key_is_valid(api_key.clone(), db.get_pool()).await {
+                Ok(valid) if valid => Outcome::Success(api_key),
+                Ok(valid) if !valid => {
+                    key_error(ApiKeyError::NotFound("API key was not found".to_owned()))
+                }
+                _ => server_error(),
+            }
+        }
+    }
+   }
+}
