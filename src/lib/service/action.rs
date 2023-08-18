0bc714a11b07bc14b53c17f3b4978f0b930a106e
Ishaaq Ziyan <ishaaqziyan@gmail.com>
2023-08-18 10:44:20 +0530

    API implementation continued

--- src/lib/service/action.rs
+++ src/lib/service/action.rs
@@ -2,6 +2,7 @@ use crate::data::model::NewClip;
 use crate::data::{query,DatabasePool,Transaction};
 use crate::service::ask;
 use crate::{Clip,ServiceError,ShortCode};
+use crate::web::api::ApiKey;
 use std::convert::TryInto;
 
 //Starts TX
@@ -49,4 +50,17 @@ pub async fn get_clip (req: ask::GetClip, pool: &DatabasePool)
     Ok(clip)
 }
     
+}
+
+pub async fn generate_api_key(pool: &DatabasePool) -> Result<ApiKey, ServiceError> {
+    let api_key = ApiKey::default();
+    Ok(query::save_api_key(api_key, pool).await?)
+}
+
+pub async fn revoke_api_key(api_key: ApiKey,pool: &DatabasePool) -> Result<query::RevocationStatus, ServiceError> {
+    Ok(query::revoke_api_key(api_key, pool).await?)
+}
+
+pub async fn api_key_is_valid(api_key: ApiKey,pool: &DatabasePool) -> Result<bool, ServiceError> {
+    Ok(query::api_key_is_valid(api_key, pool).await?)
 }
