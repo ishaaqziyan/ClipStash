326c91601197dfcae80fd43d248a920a168a0371
Ishaaq Ziyan <ishaaqziyan@gmail.com>
2023-08-18 20:11:47 +0530

    Deployed Final tests for ClipStash

--- src/lib/web/http.rs
+++ src/lib/web/http.rs
@@ -1,21 +1,13 @@
 use crate::data::AppDatabase;
-use crate::domain::clip;
-use crate::domain::clip::field::Password;
 use crate::service;
 use crate::service::action;
-use crate::web::{ctx, form, renderer::Renderer, PageError , PASSWORD_COOKIE, hitcounter::HitCounter};
+use crate::web::{ctx, form, renderer::Renderer, PageError, PASSWORD_COOKIE, hitcounter::HitCounter};
 use crate::{ServiceError, ShortCode};
 use rocket::form::{Contextual, Form};
-use rocket::futures::future::ok;
 use rocket::http::{Cookie, CookieJar, Status};
 use rocket::response::content::Html;
-use rocket::response::status::Unauthorized;
 use rocket::response::{status, Redirect};
 use rocket::{uri, State};
-use serde_json::Value;
-
-
-use super::renderer;
 
 #[rocket::get("/")]
 fn home(renderer: &State<Renderer<'_>>) -> Html<String> {
@@ -206,4 +198,76 @@ pub mod catcher {
         catchers![not_found, default, internal_error]
     }
 
+    #[cfg(test)]
+
+pub mod test {
+    use crate::data::AppDatabase;
+    use crate::test::async_runtime;
+    use crate::web::test::client;
+    use rocket::http::Status;
+
+    #[test]
+    fn gets_home() {
+        let client = client();
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+    
+    #[test]
+    fn error_on_missing_clip_id() {
+        let client = client();
+        let response = client.get("/clip/hdjjsdidof").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+
+    #[test]
+    fn requires_password_when_applicable() {
+        use crate::domain::clip::field::{Content, Expires, Password, Title};
+        use crate::service;
+        use rocket::http::{ContentType, Cookie};
+
+        let rt = async_runtime();
+
+        let client = client();
+        let db = client.rocket().state::<AppDatabase>().unwrap();
+
+        let req = service::ask::NewClip {
+            content: Content::new("content").unwrap(),
+            expires: Expires::default(),
+            password: Password::new("123".to_owned()).unwrap(),
+            title: Title::default(),
+        };
+        let clip = rt
+            .block_on(async move { service::action::new_clip(req, db.get_pool()).await })
+            .unwrap();
+
+        let response = client
+            .get(format!("/clip/{}", clip.shortcode.as_str()))
+            .dispatch();
+        assert_eq!(response.status(), Status::Unauthorized);
+        let response = client
+            .get(format!("/clip/raw/{}", clip.shortcode.as_str()))
+            .dispatch();
+        assert_eq!(response.status(), Status::Unauthorized);
+
+        let response = client
+            .post(format!("/clip/{}", clip.shortcode.as_str()))
+            .header(ContentType::Form)
+            .body("password=123")
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let response = client
+            .get(format!("/clip/raw/{}", clip.shortcode.as_str()))
+            .cookie(Cookie::new("password", "123"))
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let response = client
+            .get(format!("/clip/raw/{}", clip.shortcode.as_str()))
+            .cookie(Cookie::new("password", "abcdef"))
+            .dispatch();
+        assert_eq!(response.status(), Status::Unauthorized);
+    }
+}
 }
